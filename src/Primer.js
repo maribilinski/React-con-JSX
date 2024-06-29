343148630a71eb9b52753e16b9c77add895de15a
maribilinski <nanabilinski2004@gmail.com>
2024-06-29 15:36:22 -0400

    guardando

---
+++ src/Primer.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './Primer.css';
+
+function Primer() {
+  return (
+    <>
+      <header>
+          <h1>Hello Dojo!</h1>
+          <h2>Things I need to do:</h2>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+      </header>
+    </>
+  );
+}
+
+export default Primer;
--- src/Primer.test.js
+++ src/Primer.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Primer from './Primer';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Primer />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
